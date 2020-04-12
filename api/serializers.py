e1a3cc865abb5c8299fec3ab72a4dc640c276833
jonatasilva <jonatas.iw@gmail.com>
2020-04-12 14:28:03 -0300

    chore: serializer manytomany CLiente/Nota

--- api/serializers.py
+++ api/serializers.py
@@ -1,21 +1,11 @@
 from rest_framework import serializers
 from core.models import Chapa,Nota,Servico,Cliente
 
+
 class ChapaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chapa
         fields = ('id', 'nome', 'valor','estoque','obs','created_at','uploaded_at')
-
-class NotaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Nota
-        fields = ('id', 'desconto', 'numero','obs','servico','created_at','uploaded_at')
-
-class ClienteSerializer(serializers.ModelSerializer):
-    nota = NotaSerializer(read_only=True, many=True)
-    class Meta:
-        model = Cliente
-        fields = ('id', 'nome', 'email','telefone','cnpj','cpf','rua','bairro','numero','cidade','estado','cep','created_at','uploaded_at',nota)
 
 class ServicoSerializer(serializers.ModelSerializer):
     cliente = serializers.StringRelatedField()
@@ -23,4 +13,17 @@ class ServicoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Servico
         fields = ('id', 'nome', 'cliente','chapa','quantidade','created_at','uploaded_at')
+
+class NotaSerializer(serializers.ModelSerializer):
+    servico = ServicoSerializer(many=True)
+    class Meta:
+        model = Nota
+        fields = ('id', 'desconto', 'numero','obs','servico','created_at','uploaded_at')
+
+class ClienteSerializer(serializers.ModelSerializer):
+    nota = NotaSerializer(many=True)
+
+    class Meta:
+        model = Cliente
+        fields = ('id', 'nome', 'email','telefone','cnpj','cpf','rua','bairro','numero','cidade','estado','cep','created_at','uploaded_at','nota')
 
