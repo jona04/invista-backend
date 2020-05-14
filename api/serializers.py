2f69437a3a782ce233749710933911b3b0e7b3ee
jonatasilva <jonatas.iw@gmail.com>
2020-05-14 15:41:29 -0300

    concertar serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from core.models import Chapa, Nota, Servico, Cliente
 
+
 class ChapaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chapa
