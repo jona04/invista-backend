2f3df66ef5da0c22ccf6cfd084d6d607f16df43e
jonatasilva <jonatas.iw@gmail.com>
2022-10-13 13:25:08 -0300

    add docker

--- invista/settings.py
+++ invista/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         "USER": "postgres",
         "PASSWORD": "a",
-        "HOST": "localhost",  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        "HOST": "db",  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         "PORT": 5432,  # Set to empty string for default.
     }
     # 'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
