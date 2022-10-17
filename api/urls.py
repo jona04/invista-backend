b8bc8abd6ef482002cc2d0749774cd27f9789449
Jonatas O <jonatas.oliveira@optimumsupply.com.br>
2022-10-17 15:26:01 -0300

    lint

--- api/urls.py
+++ api/urls.py
@@ -1,22 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-from . import views
-
 
 router = routers.DefaultRouter()
-router.register(r"chapa", views.ChapaViewSet)
-router.register(r"cliente", views.ClienteViewSet)
-router.register(r"servico", views.ServicoViewSet)
-router.register(r"nota", views.NotaViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
